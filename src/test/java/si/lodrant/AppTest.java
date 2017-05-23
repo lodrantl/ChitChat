5738e7c91c755dac5fded5368e23e0ebd6c30d9f
lenarttreven <lenart.treven@student.fmf.uni-lj.si>
2017-05-23 16:41:13 +0200

    tests

--- src/test/java/si/lodrant/AppTest.java
+++ src/test/java/si/lodrant/AppTest.java
@@ -2,7 +2,9 @@ package si.lodrant;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.post;
+import static io.restassured.RestAssured.delete;
 import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
 
 import org.jooby.test.JoobyRule;
 import org.junit.Rule;
@@ -48,51 +50,206 @@ public class AppTest {
 
 	@Test
 	public void addEmptyUser() {
-		// TODO
+		post("/users?username=").then()
+			.assertThat()
+			.body("status", equalTo("Bad Request(400): Cannot log you in if I do not know who you are (parameter missing)"))
+			.statusCode(400)
+			.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
 	public void addTakenUser() {
-		// TODO
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+			
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("Forbidden(403): User already exists"))
+			.statusCode(403)
+			.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
-	public void deleteUser() {
-		// TODO
-	}
-
-	@Test
-	public void recieveEmptyMessage() {
-		// TODO
+	public void deleteExsistingUser() {
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		
+		delete("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged out"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		
+		get("/users").then()
+			.assertThat()
+			.body(equalTo("[]"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		
 	}
 	
 	@Test
-	public void recieveWithoutLogin() {
-		// TODO
+	public void deleteNonExsistingUser() {
+		delete("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User didn't exist in the first place."))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+	}
+	
+	@Test
+	public void deleteNoParameterUser() {
+		delete("/users?username=").then()
+			.assertThat()
+			.body("status", equalTo("Bad Request(400): Cannot delete you if I do not know who you are (parameter missing)"))
+			.statusCode(400)
+			.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
-	public void sendAndRecieveMessage() {
-		// TODO
+	public void sendMessage() {
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		
+		post("/users?username=mouse").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		given()
+		    .body("{\"global\":false, \"recipient\":\"mouse\", \"text\":\"bambam\"}")
+		    .contentType("application/json").
+		when()
+			.post("/messages?username=miki").
+		then()
+			.assertThat()
+			.body("status", equalTo("Message sent"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+	}
+	
+	@Test
+	public void sendWithoutLogin() {
+	post("/users?username=mouse").then()
+		.assertThat()
+		.body("status", equalTo("User logged in"))
+		.statusCode(200)
+		.contentType("application/json;charset=UTF-8");
+	
+	given()
+	    .body("{\"global\":false, \"recipient\":\"mouse\", \"text\":\"bambam\"}")
+	    .contentType("application/json").
+	when()
+		.post("/messages?username=miki").
+	then()
+		.assertThat()
+		.body("status", equalTo("Unauthorized(401): You are not logged in."))
+		.statusCode(401)
+		.contentType("application/json;charset=UTF-8");
 	}
 
+
 	@Test
-	public void sendAndRecieveGlobalMessage() {
-		// TODO
+	public void sendGlobalMessage() {
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		
+		post("/users?username=mouse").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		given()
+		    .body("{\"global\":true, \"recipient\":\"\", \"text\":\"bambam\"}")
+		    .contentType("application/json").
+		when()
+			.post("/messages?username=miki").
+		then()
+			.assertThat()
+			.body("status", equalTo("Message sent"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
 	public void sendWithoutUsername() {
-		// TODO
+		post("/users?username=mouse").then()
+		.assertThat()
+		.body("status", equalTo("User logged in"))
+		.statusCode(200)
+		.contentType("application/json;charset=UTF-8");
+	
+	given()
+	    .body("{\"global\":false, \"recipient\":\"mouse\", \"text\":\"bambam\"}")
+	    .contentType("application/json").
+	when()
+		.post("/messages?username=").
+	then()
+		.assertThat()
+		.body("status", equalTo("Bad Request(400): Cannot send messages if I do not know who you are (parameter missing)"))
+		.statusCode(400)
+		.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
 	public void sendWithoutRecipient() {
-		// TODO
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+	
+		post("/users?username=mouse").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		given()
+		    .body("{\"global\":false, \"recipient\":\"\", \"text\":\"bambam\"}")
+		    .contentType("application/json").
+		when()
+			.post("/messages?username=miki").
+		then()
+			.assertThat()
+			.body("status", equalTo("Bad Request(400): Cannot send messages if I do not tell recipient."))
+			.statusCode(400)
+			.contentType("application/json;charset=UTF-8");
 	}
 
 	@Test
-	public void sendWithoutLogin() {
-		// TODO
+	public void sendToUnactiveUser() {
+		post("/users?username=miki").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+	
+		post("/users?username=mouse").then()
+			.assertThat()
+			.body("status", equalTo("User logged in"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
+		given()
+		    .body("{\"global\":false, \"recipient\":\"pac\", \"text\":\"bambam\"}")
+		    .contentType("application/json").
+		when()
+			.post("/messages?username=miki").
+		then()
+			.assertThat()
+			.body("status", equalTo("Message sent"))
+			.statusCode(200)
+			.contentType("application/json;charset=UTF-8");
 	}
 }
